e49586a1e881692cf4f6e172e006cdca7c02d34f
gino <gozambelli1@gmail.com>
2023-12-30 13:18:50 +0000

    updated pc.py file

--- pc.py
+++ pc.py
@@ -10,12 +10,34 @@ min_characters = 7
 special_chars = ["!", "?", "#", "@", "$", "*"]
 numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-passwords = ["helloworld", "imchange69",
-             "ilovecookies!23", "python1@gmail.com"]
+# Function to check password strength
 
-HAS_7_CHARS = False
-HAS_SPECIAL_CHARS = False
-HAS_NUMBERS = False
+
+def check_password_strength(password):
+    # Checking the length of the password
+    if len(password) >= min_characters:
+        HAS_7_CHARS = True
+    else:
+        HAS_7_CHARS = False
+
+    # Checking for special characters
+    HAS_SPECIAL_CHARS = any(char in special_chars for char in password)
+
+    # Checking for numbers
+    HAS_NUMBERS = any(char.isdigit() for char in password)
+
+    # Provide feedback
+    if HAS_7_CHARS and HAS_SPECIAL_CHARS and HAS_NUMBERS:
+        print("Your password is strong.")
+    else:
+        if not HAS_7_CHARS:
+            print("Your password should be at least 7 characters long.")
+        if not HAS_SPECIAL_CHARS:
+            print(
+                "Your password should include at least one special character (!, ?, #, @, $, *).")
+        if not HAS_NUMBERS:
+            print("Your password should include at least one number.")
+
 
 # State password requirements
 print("\nWelcome to the password checker!\n")
@@ -25,6 +47,18 @@ print("- At least 7 characters long\n- Include at least 1 special character\n- I
 # Users password
 users_password = input("Enter your password: ")
 
-# check requirements (lenght, special characters, number)
-# provide feeedback
-# retry option
+# Check the password strength
+check_password_strength(users_password)
+
+# Retry option
+while True:
+    retry = input(
+        "\nWould you like to try another password? (yes/no): ").lower()
+    if retry == "yes":
+        users_password = input("Enter your password: ")
+        check_password_strength(users_password)
+    elif retry == "no":
+        print("Thank you for using the password checker!")
+        break
+    else:
+        print("Please enter 'yes' or 'no'.")
